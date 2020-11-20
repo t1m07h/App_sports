b2fe8842ed1c33058e0a8062ce34b08fd60aedb2
t1m07h <timotheebenard@protonmail.com>
2020-11-20 11:59:57 +0100

    user_flow

--- app/src/main/java/com/example/app_sports/home_activity/fragments/user_activities/UserActivitiesFragment.kt
+++ app/src/main/java/com/example/app_sports/home_activity/fragments/user_activities/UserActivitiesFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.app_sports.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class UserActivitiesFragment : Fragment() {
+
+	private lateinit var auth: FirebaseAuth
+	private var db_ref = Firebase.database.getReference("user")
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +23,11 @@ class UserActivitiesFragment : Fragment() {
 	): View? {
 		val view = inflater.inflate(R.layout.fragment_user_activities, container, false)
 		return view
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		val recyclerView = view.findViewById<RecyclerView>(R.id.user_recycler_view)
 	}
 }
