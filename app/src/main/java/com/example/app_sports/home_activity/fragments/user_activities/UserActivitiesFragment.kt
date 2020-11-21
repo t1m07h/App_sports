0ee49ddc040dd31af482bbb48c3e3c2dded31d26
t1m07h <timotheebenard@protonmail.com>
2020-11-21 22:09:59 +0100

    userActivitiesFragment

--- app/src/main/java/com/example/app_sports/home_activity/fragments/user_activities/UserActivitiesFragment.kt
+++ app/src/main/java/com/example/app_sports/home_activity/fragments/user_activities/UserActivitiesFragment.kt
@@ -6,11 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.app_sports.Model.ActivitiesModel.ActivitiesData
 import com.example.app_sports.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class UserActivitiesFragment : Fragment() {
 
@@ -29,5 +37,26 @@ class UserActivitiesFragment : Fragment() {
 		super.onViewCreated(view, savedInstanceState)
 
 		val recyclerView = view.findViewById<RecyclerView>(R.id.user_recycler_view)
+		val emptyRvText = view.findViewById<TextView>(R.id.empty_user_rv)
+		// TODO: 21/11/20 set the recyclerView adapter
+		emptyRvText.visibility = TextView.INVISIBLE
+
+		val activitiesListener = object : ValueEventListener {
+			override fun onDataChange(snapshot: DataSnapshot) {
+				val activities = snapshot.getValue<List<ActivitiesData>>()
+				if (activities != null) {
+					// TODO: 21/11/20 update recyclerView adapter
+				} else {
+					emptyRvText.visibility = TextView.VISIBLE
+					recyclerView.visibility = RecyclerView.INVISIBLE
+				}
+			}
+
+			override fun onCancelled(error: DatabaseError) {
+				Toast.makeText(requireContext(), error.toString(), Toast.LENGTH_SHORT).show()
+			}
+		}
+
+		db_ref.addValueEventListener(activitiesListener)
 	}
 }
