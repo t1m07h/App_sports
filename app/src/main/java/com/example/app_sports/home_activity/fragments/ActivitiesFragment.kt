172eb5df5eab24304c488138841991d618ad307f
t1m07h <timotheebenard@protonmail.com>
2020-11-30 20:02:35 +0100

    ActivitiesFragment

--- app/src/main/java/com/example/app_sports/home_activity/fragments/ActivitiesFragment.kt
+++ app/src/main/java/com/example/app_sports/home_activity/fragments/ActivitiesFragment.kt
@@ -1,4 +1,19 @@
 package com.example.app_sports.home_activity.fragments
 
-class ActivitiesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ActivitiesFragment : Fragment(){
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+//		val layout =
+		// TODO: 30/11/20 select the layout in function of the fragment called 
+		val view = inflater.inflate(layout, container, false)
+	}
 }
